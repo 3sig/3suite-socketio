79675775c9d1800af5c432bbd59919ea98f8cc65
Aaron Santiago <me@aaron.work>
2025-03-02 18:48:44 -0500

    initial functioning commit

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 import config from '3lib-config';
+import { Server } from "socket.io";
 
 config.init();
-console.log(config.get()["test"]);
+
+const io = new Server({
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  socket.onAny((ev, message) => {
+    console.log(ev, message);
+    socket.broadcast.emit(ev, message);
+  })
+});
+
+let port = 3000;
+if (config.get().port) {
+  port = config.get().port;
+}
+
+io.listen(port);
+
+console.log("Server started on port", port);
